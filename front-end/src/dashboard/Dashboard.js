5c9568db4d14905346b27be83652dcb189f9031e
Scarlett Alston <scarlett938@gmail.com>
2022-03-10 16:20:43 -0800

    updated dashboard.js to include today's rez.

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -4,12 +4,6 @@ import ErrorAlert from "../layout/ErrorAlert";
 import { today, previous, next } from "../utils/date-time";
 import { Link, useLocation } from "react-router-dom";
 
-/**
- * Defines the dashboard page.
- * @param date
- *  the date for which the user wants to view reservations.
- * @returns {JSX.Element}
- */
 function Dashboard() {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
@@ -23,7 +17,7 @@ function Dashboard() {
   const previousDate = previous(date);
   const nextDate = next(date);
 
-  useEffect(loadDashboard(date), [date]);
+  useEffect(() => loadDashboard(date), [date]);
 
   function loadDashboard(date) {
     const abortController = new AbortController();
@@ -40,8 +34,40 @@ function Dashboard() {
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for date</h4>
       </div>
+      <button type="button" class="btn btn-outline-secondary">
+        Previous
+      </button>
+      <button className="btn btn-primary">Today</button>
+      <button type="button" class="btn btn-outline-secondary">
+        Next
+      </button>
+      <div>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Time</th>
+              <th scope="col">Name</th>
+              <th scope="col">Party Size</th>
+              <th scope="col">Mobile Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reservations.map((reservation) => {
+              return (
+                <tr key={reservation.reservation_id}>
+                  <td>{reservation.reservation_time}</td>
+                  <td>
+                    {reservation.first_name} {reservation.last_name}
+                  </td>
+                  <td>{reservation.people}</td>
+                  <td>{reservation.mobile_number}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
     </main>
   );
 }
