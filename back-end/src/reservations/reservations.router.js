711a19fc9261e65d8c96787fd1af8b894f5bdb69
Scarlett Alston <scarlett938@gmail.com>
2022-03-22 23:08:03 -0700

    added status update route

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -13,4 +13,9 @@ router
   .get(controller.getReservation)
   .all(methodNotAllowed)
 
+router
+  .route("/:reservation_id/status")
+  .put(controller.updateStatus)
+  .all(methodNotAllowed)
+
 module.exports = router;
