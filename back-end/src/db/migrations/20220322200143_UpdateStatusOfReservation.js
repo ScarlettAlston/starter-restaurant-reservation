ee175cf5bd64bdf843bf1fb207450cabc7df112e
Scarlett Alston <scarlett938@gmail.com>
2022-03-22 20:52:48 -0700

    updated migrations

--- back-end/src/db/migrations/20220322200143_UpdateStatusOfReservation.js
+++ back-end/src/db/migrations/20220322200143_UpdateStatusOfReservation.js
@@ -1,8 +1,14 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.table('reservations', table => {
+    table.string("status", null).notNullable().defaultTo("booked")
+  })
+
+
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.table("reservations", (table) => {
+    table.dropColumn("status");
+  });
 };
